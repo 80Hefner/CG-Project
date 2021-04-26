bdb04cafe6d06ff02f2c8e98a255c320556a3690
80Hefner <luisenesousa@gmail.com>
2021-04-26 02:58:36 +0100

    erro ao guardar numero de vertices de uma vbo que poderia dar problemas

--- Fase3/src/Engine/utils/model.h
+++ Fase3/src/Engine/utils/model.h
@@ -12,19 +12,19 @@ using namespace std;
 class Model {
     private:
         GLuint vbo_ind;
-        GLuint vertice_count;
+        GLsizei vertice_count;
     public:
         Model() {
             this->vbo_ind = 0;
             this->vertice_count = 0;
         };
-        Model(GLuint vbo_ind, GLuint vertice_count) {
+        Model(GLuint vbo_ind, GLsizei vertice_count) {
             this->vbo_ind = vbo_ind;
             this->vertice_count = vertice_count;
         };
         
         GLuint getVBOInd() {return this->vbo_ind;};
-        GLuint getVerticeCount() {return this->vertice_count;};
+        GLsizei getVerticeCount() {return this->vertice_count;};
 };
 
 #endif //MODEL_H
