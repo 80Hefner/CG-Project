25125c07644f2e9e3ab2619a0ce257ba900bae45
80Hefner <luisenesousa@gmail.com>
2021-05-31 00:11:32 +0100

    script intalacao linux

---
+++ install.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+# Script para Linux
+
+# cmake e cmake-qt gui
+sudo apt-get install cmake
+sudo apt-get cmake-qt-gui
+
+# freeglut
+sudo apt-get install freeglut3-dev
+
+# OpenGL
+sudo apt-get install mesa-utils
+
+# libxmu e libxi
+sudo apt-get install libxmu-dev libxi-dev
+
+# GLEW
+sudo apt-get install libglew-dev
+
+# DevIL
+sudo apt-get install libdevil-dev
+
+
